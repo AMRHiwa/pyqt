1067e155d567bcdee4da238e97ad9d6b6034c178
Hiwa Azizi <azizi.mr1377@gmail.com>
2022-03-08 16:33:36 +0330

    edit main

--- part3 notepad/main.py
+++ part3 notepad/main.py
@@ -1,4 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
+from PyQt6.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
+from PyQt6.QtCore import QFileInfo
 import sys
 
 from NotePadApp import Ui_MainWindow
@@ -9,8 +11,40 @@ class NotePadWindow(QMainWindow,Ui_MainWindow):
         self.setupUi(self)
         self.actionSave.triggered.connect(self.save_file)
         self.actionOpen.triggered.connect(self.open_file)
+        self.actionNew.triggered.connect(self.new_file)
+        self.actionPrint.triggered.connect(self.print_file)
+        self.actionPreview.triggered.connect(self.preview)
+        self.actionExport_PDF.triggered.connect(self.export_pdf)
+        self.actionExit.triggered.connect(self.quit)
         self.show()
 
+    def quit(self):
+        self.close()
+
+    def export_pdf(self):
+        file, ok = QFileDialog.getSaveFileName(self, "Export pdf", "PDF file (.pdf) ;; All files ()")
+        if file != "":
+            if QFileInfo(file).suffix() == "": file += ".pdf"
+            printer = QPrinter(QPrinter.PrinterMode.HighResolution)
+            printer.setOutputFormat(QPrinter.OutputFormat.PdfFormat)
+            printer.setOutputFileName(file)
+            self.textEdit.document().print(printer)
+
+    def preview(self):
+        printer = QPrinter(QPrinter.PrinterMode.HighResolution)
+        dialog = QPrintPreviewDialog(printer, self)
+        dialog.paintRequested.connect(self.print_preview)
+        dialog.exec()
+
+    def print_preview(self, printer):
+        self.textEdit.print(printer)
+
+    def print_file(self):
+        printer = QPrinter(QPrinter.PrinterMode.HighResolution)
+        dialog = QPrintDialog(printer)
+
+        if dialog.exec() == QPrintDialog.DialogCode.Accepted:
+            self.textEdit.print(printer)
 
     def save_file(self):
         filename = QFileDialog.getSaveFileName(self, "Save File")
@@ -29,6 +63,24 @@ class NotePadWindow(QMainWindow,Ui_MainWindow):
             text = f.read()
             self.textEdit.setText(text)
             QMessageBox.about(self, "Open file", "File has been opened")
+
+    def maybe_saved(self):
+        if not self.textEdit.document().isModified():
+            return True
+        ret = QMessageBox.warning(self, "Application", "The document has been modified\nDo you want to save your changes ?",
+                        QMessageBox.StandardButton.Save|QMessageBox.StandardButton.Discard|QMessageBox.StandardButton.Cancel)
+
+        if ret == QMessageBox.StandardButton.Save:
+            return self.save_file()
+        if ret == QMessageBox.StandardButton.Cancel:
+            return False
+
+        return True
+
+    def new_file(self):
+        if self.maybe_saved():
+            self.textEdit.clear()
+
 app = QApplication(sys.argv)
 notepad = NotePadWindow()
 sys.exit(app.exec())
