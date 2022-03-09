563779764e4fe90ba2cf8ddf5873f8a855a12553
Hiwa Azizi <azizi.mr1377@gmail.com>
2022-03-09 22:54:09 +0330

    add part4 database handling

---
+++ part4 database handling/1.install_mysql_module.py
@@ -0,0 +1,4 @@
+# for installing mysql module in python we must use under order in cmd
+# pip install mysql-connector-python
+# then we need install one software like wampserver
+# for installing wampserver, we can go to wampserver.com
